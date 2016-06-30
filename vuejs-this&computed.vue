499136d28940d0e82df32035b3c34735f8b04dda
郑灵华 <Serena_zlh@163.com>
2016-06-30 23:35:54 +0800

    vue.js在没有路由的情况修改绑定数据

---
+++ vuejs-this&computed.vue
@@ -0,0 +1,38 @@
+<script src="https://npmcdn.com/vue/dist/vue.js"></script>
+
+<div id="app">
+    {{ message }}
+	<input v-model="message"/>
+	<input type="button" v-on:click="get"/>
+</div>
+
+<script>
+new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+		methods: {
+			get: function(){
+				//this.message = "he"
+				console.log('nihao')
+				this.setMsg='wa'
+				// this.setMsg
+			}
+		},
+		computed: {
+			getMsg:function() {
+				return this.message
+			},
+			setMsg: {
+				get: function(){
+					//return this.message=this.message+"lala"
+					return this.message+'1212'//没有结果
+				},
+				set: function(newmsg) {
+					this.message = newmsg
+				}
+			}
+		},
+})
+</script>
